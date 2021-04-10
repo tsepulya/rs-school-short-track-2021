2635237018389b9b2c9d52152849436fea6579b7
tsepulya <tsepeleva_anna@mail.ru>
2021-04-10 12:19:49 +0300

    feat: add encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const strNew = [];
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else if (count === 1) {
+      strNew.push(str[i]);
+    } else {
+      strNew.push(`${count}${str[i]}`);
+      count = 1;
+    }
+  }
+  return strNew.join('');
 }
 
 module.exports = encodeLine;
